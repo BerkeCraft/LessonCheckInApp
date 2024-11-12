caddf091f386331f484bf3a60e52f84556701847
BerkeCraft <berkecora@hotmail.com>
2024-11-12 13:22:39 +0300

    Initial Commit

---
+++ LessonCheckIn/LessonCheckInApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessonCheckInApp.swift
+//  LessonCheckIn
+//
+//  Created by Berke Cora on 12.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct LessonCheckInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
